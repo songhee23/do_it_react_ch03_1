6e7dc014fbcd990375aae0572e729f1056cdfb8d
unknown <ereteyey@naver.com>
2025-03-20 16:43:14 +0900

    .

---
+++ src/pages/Icon.tsx
@@ -0,0 +1,11 @@
+export default function Icon() {
+  return (
+    <div>
+      <h3>Icon</h3>
+      <span className="material-icons">home</span>
+      <span className="material-icons">check_circle_outline</span>
+      <span className="material-icons">folder</span>
+      <span className="material-icons">check</span>
+    </div>
+  )
+}
